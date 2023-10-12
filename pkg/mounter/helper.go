1bb8dd79144ac7b75de69213cddf2deb71a4f5ad
tydra-wang <wjunjie96@outlook.com>
2023-10-12 10:20:03 +0800

    feat(nas): support cpfs unmount using mount.cpfs if version>1.1-5

--- pkg/mounter/helper.go
+++ pkg/mounter/helper.go
@@ -1,6 +1,10 @@
 package mounter
 
-import "strings"
+import (
+	"strings"
+
+	mountutils "k8s.io/mount-utils"
+)
 
 // Copy from https://github.com/kubernetes/mount-utils/blob/41e8de37ef8a3782f9cd6c915699b98b2b24b2c4/mount_helper_unix.go#L164
 func SplitMountOptions(s string) []string {
@@ -13,4 +17,17 @@ func SplitMountOptions(s string) []string {
 		return r == ',' && !inQuotes
 	})
 	return list
+}
+
+func GetMountpointType(mounter mountutils.Interface, path string) (string, bool, error) {
+	mountpoints, err := mounter.List()
+	if err != nil {
+		return "", false, err
+	}
+	for _, mp := range mountpoints {
+		if mp.Path == path {
+			return mp.Type, true, nil
+		}
+	}
+	return "", false, nil
 }
--- pkg/nas/mounter.go
+++ pkg/nas/mounter.go
@@ -1,7 +1,15 @@
 package nas
 
 import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"time"
+
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/mounter"
+	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/utils"
+	log "github.com/sirupsen/logrus"
 	mountutils "k8s.io/mount-utils"
 )
 
@@ -17,6 +25,94 @@ func (m *NasMounter) Mount(source string, target string, fstype string, options 
 	default:
 		return m.Interface.Mount(source, target, fstype, options)
 	}
+}
+
+func (m *NasMounter) Unmount(target string) error {
+	cpfs, err := m.isCpfsMountpoint(target)
+	if err != nil {
+		return err
+	}
+	if cpfs {
+		return m.unmountCpfs(target)
+	}
+	return m.Interface.Unmount(target)
+}
+
+func (m *NasMounter) UnmountWithForce(target string, umountTimeout time.Duration) error {
+	cpfs, err := m.isCpfsMountpoint(target)
+	if err != nil {
+		return err
+	}
+	if cpfs {
+		return m.unmountCpfs(target)
+	}
+	return m.Interface.(mountutils.MounterForceUnmounter).UnmountWithForce(target, umountTimeout)
+}
+
+func (m *NasMounter) isCpfsMountpoint(target string) (bool, error) {
+	fstype, found, err := mounter.GetMountpointType(m, target)
+	if err != nil {
+		return false, fmt.Errorf("failed to determine fstype: %w", err)
+	}
+	if !found {
+		return false, fmt.Errorf("not a mountpoint: %q", target)
+	}
+	return fstype == "gpfs", nil
+}
+
+func (m *NasMounter) unmountCpfs(target string) error {
+	cmd := "mount.cpfs --version"
+	out, err := utils.ConnectorRun(cmd)
+	if out != "" {
+		log.Infof("ConnectorRun: %s, output: %q", cmd, out)
+	}
+	if err != nil {
+		return fmt.Errorf("check mount.cpfs version: %w", err)
+	}
+	needHelper, err := isCpfsVersionLargerThen1_1_5(out)
+	if err != nil {
+		return err
+	}
+	if !needHelper {
+		return m.Interface.Unmount(target)
+	}
+
+	// use mount.cpfs to unmount
+	cmd = fmt.Sprintf("mount.cpfs -u %s", target)
+	out, err = utils.ConnectorRun(cmd)
+	if out != "" {
+		log.Infof("ConnectorRun: %s, output: %q", cmd, out)
+	}
+	if err != nil {
+		return fmt.Errorf("unmount cpfs: %w", err)
+	}
+	return nil
+}
+
+func isCpfsVersionLargerThen1_1_5(versionOutput string) (bool, error) {
+	re := regexp.MustCompile(`Version: (\d+)\.(\d+)-(\d+)$`)
+	match := re.FindStringSubmatch(versionOutput)
+	if len(match) == 4 {
+		majorVersion, err := strconv.Atoi(match[1])
+		valid := err == nil
+		minorVersion, err := strconv.Atoi(match[2])
+		valid = valid && err == nil
+		revision, err := strconv.Atoi(match[3])
+		valid = valid && err == nil
+		if !valid {
+			return false, errors.New("unexpected mount.cpfs version format")
+		}
+		baseVersion := []int{1, 1, 5}
+		for i, v := range []int{majorVersion, minorVersion, revision} {
+			if v > baseVersion[i] {
+				return true, nil
+			} else if v < baseVersion[i] {
+				return false, nil
+			}
+		}
+		return false, nil
+	}
+	return false, errors.New("unexpected output of mount.cpfs --version")
 }
 
 func NewNasMounter() mountutils.Interface {
